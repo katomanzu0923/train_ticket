5ae6181f00ccabce8e5c3167c4441aed42d00c48
katomanzu0923 <katomanzu86@gmail.com>
2020-10-01 14:35:32 +0900

    revise a layout in top (2)

---
+++ app/javascript/select.js
@@ -0,0 +1,163 @@
+//ログインボタン（日本語）
+$(function() {
+	$(".login_jp_path").on("mouseover", function() {
+		$(this).css({"filter": "opacity(0%)"})
+		$(".login_jp_click_path").css({"filter": "opacity(100%)"})
+		$(".registration_jp_path").css({"filter": "opacity(30%)"})
+		$(".no_login_jp_path").css({"filter": "opacity(30%)"})
+	}).on("mouseout", function() {
+		$(this).css({"filter": "opacity(100%)"})
+		$(".registration_jp_path").css({"filter": "opacity(100%)"})
+		$(".no_login_jp_path").css({"filter": "opacity(100%)"})
+		$(".no_login_click_jp_path").css({"filter": "opacity(0%)"})
+		$(".top_jp_click_path").css({"filter": "opacity(100%)"})
+		$(".click_jp_top").fadeOut(500);
+	})
+});
+//登録ボタン（日本語）
+$(function() {
+	$(".registration_jp_path").on("mouseover", function() {
+		$(this).css({"filter": "opacity(0%)"})
+		$(".registration_jp_click_path").css({"filter": "opacity(100%)"})
+		$(".login_jp_path").css({"filter": "opacity(30%)"})
+		$(".login_jp_click_path").css({"filter": "opacity(0%)"})
+		$(".no_login_jp_path").css({"filter": "opacity(30%)"})
+	}).on("mouseout", function() {
+		$(this).css({"filter": "opacity(100%)"})
+		$(".login_jp_path").css({"filter": "opacity(100%)"})
+		$(".registration_jp_click_path").css({"filter": "opacity(0%)"})
+		//$(".registration_jp_click_path").css({"filter": "opacity(0%)"})
+		$(".no_login_jp_path").css({"filter": "opacity(100%)"})
+		$(".no_login_click_jp_path").css({"filter": "opacity(0%)"})
+	})
+});
+//ノーログインボタン（日本語）
+$(function() {
+	$(".no_login_jp_path").on("mouseover", function() {
+		$(this).css({"filter": "opacity(0%)"})
+		$(".no_login_click_jp_path").css({"filter": "opacity(100%)"})
+		$(".login_jp_path").css({"filter": "opacity(30%)"})
+		$(".login_jp_click_path").css({"filter": "opacity(0%)"})
+		$(".registration_jp_path").css({"filter": "opacity(30%)"})
+		$(".registration_click_jp_path").css({"filter": "opacity(0%)"})
+	}).on("mouseout", function() {
+		$(this).css({"filter": "opacity(100%)"})
+		$(".login_jp_path").css({"filter": "opacity(100%)"})
+		$(".registration_jp_click_path").css({"filter": "opacity(0%)"})
+		$(".registration_jp_path").css({"filter": "opacity(100%)"})
+		$(".no_login_click_jp_path").css({"filter": "opacity(0%)"})
+	})
+});
+//ログインボタン（英語）
+$(function() {
+	$(".login_en_path").on("mouseover", function() {
+		$(this).css({"filter": "opacity(0%)"})
+		$(".login_en_click_path").css({"filter": "opacity(100%)"})
+		$(".registration_en_path").css({"filter": "opacity(30%)"})
+		$(".no_login_en_path").css({"filter": "opacity(30%)"})
+	}).on("mouseout", function() {
+		$(this).css({"filter": "opacity(100%)"})
+		$(".registration_en_path").css({"filter": "opacity(100%)"})
+		$(".no_login_en_path").css({"filter": "opacity(100%)"})
+		$(".no_login_click_en_path").css({"filter": "opacity(0%)"})
+		$(".top_jp_click_path").css({"filter": "opacity(100%)"})
+		$(".click_jp_top").fadeOut(500);
+	})
+});
+//登録ボタン（英語）
+$(function() {
+	$(".registration_en_path").on("mouseover", function() {
+		$(this).css({"filter": "opacity(0%)"})
+		$(".registration_en_click_path").css({"filter": "opacity(100%)"})
+		$(".login_en_path").css({"filter": "opacity(30%)"})
+		$(".login_en_click_path").css({"filter": "opacity(0%)"})
+		$(".no_login_en_path").css({"filter": "opacity(30%)"})
+	}).on("mouseout", function() {
+		$(this).css({"filter": "opacity(100%)"})
+		$(".login_en_path").css({"filter": "opacity(100%)"})
+		$(".registration_en_click_path").css({"filter": "opacity(0%)"})
+		$(".no_login_en_path").css({"filter": "opacity(100%)"})
+		$(".no_login_click_en_path").css({"filter": "opacity(0%)"})
+	})
+});
+//ノーログインボタン(英語)
+$(function() {
+	$(".no_login_en_path").on("mouseover", function() {
+		$(this).css({"filter": "opacity(0%)"})
+		$(".no_login_click_en_path").css({"filter": "opacity(100%)"})
+		$(".login_en_path").css({"filter": "opacity(30%)"})
+		$(".login_en_click_path").css({"filter": "opacity(0%)"})
+		$(".registration_en_path").css({"filter": "opacity(30%)"})
+		$(".registration_click_en_path").css({"filter": "opacity(0%)"})
+	}).on("mouseout", function() {
+		$(this).css({"filter": "opacity(100%)"})
+		$(".login_en_path").css({"filter": "opacity(100%)"})
+		$(".registration_en_click_path").css({"filter": "opacity(0%)"})
+		$(".registration_en_path").css({"filter": "opacity(100%)"})
+		$(".no_login_click_en_path").css({"filter": "opacity(0%)"})
+	})
+});
+//ログインボタン（中国語）
+$(function() {
+	$(".login_zh_path").on("mouseover", function() {
+		$(this).css({"filter": "opacity(0%)"})
+		$(".login_zh_click_path").css({"filter": "opacity(100%)"})
+		$(".registration_zh_path").css({"filter": "opacity(30%)"})
+		$(".no_login_zh_path").css({"filter": "opacity(30%)"})
+	}).on("mouseout", function() {
+		$(this).css({"filter": "opacity(100%)"})
+		$(".registration_zh_path").css({"filter": "opacity(100%)"})
+		$(".no_login_zh_path").css({"filter": "opacity(100%)"})
+		$(".no_login_click_zh_path").css({"filter": "opacity(0%)"})
+		$(".top_zh_click_path").css({"filter": "opacity(100%)"})
+		$(".click_jp_top").fadeOut(500);
+	})
+});
+//登録ボタン（中国語）
+$(function() {
+	$(".registration_zh_path").on("mouseover", function() {
+		$(this).css({"filter": "opacity(0%)"})
+		$(".registration_zh_click_path").css({"filter": "opacity(100%)"})
+		$(".login_zh_path").css({"filter": "opacity(30%)"})
+		$(".login_zh_click_path").css({"filter": "opacity(0%)"})
+		$(".no_login_zh_path").css({"filter": "opacity(30%)"})
+	}).on("mouseout", function() {
+		$(this).css({"filter": "opacity(100%)"})
+		$(".login_zh_path").css({"filter": "opacity(100%)"})
+		$(".registration_zh_click_path").css({"filter": "opacity(0%)"})
+		$(".no_login_zh_path").css({"filter": "opacity(100%)"})
+		$(".no_login_click_zh_path").css({"filter": "opacity(0%)"})
+	})
+});
+//ノーログインボタン（中国語）
+$(function() {
+	$(".no_login_zh_path").on("mouseover", function() {
+		$(this).css({"filter": "opacity(0%)"})
+		$(".no_login_click_zh_path").css({"filter": "opacity(100%)"})
+		$(".login_zh_path").css({"filter": "opacity(30%)"})
+		$(".login_zh_click_path").css({"filter": "opacity(0%)"})
+		$(".registration_zh_path").css({"filter": "opacity(30%)"})
+		$(".registration_click_zh_path").css({"filter": "opacity(0%)"})
+	}).on("mouseout", function() {
+		$(this).css({"filter": "opacity(100%)"})
+		$(".login_zh_path").css({"filter": "opacity(100%)"})
+		$(".registration_zh_click_path").css({"filter": "opacity(0%)"})
+		$(".registration_zh_path").css({"filter": "opacity(100%)"})
+		$(".no_login_click_zh_path").css({"filter": "opacity(0%)"})
+	})
+});
+//ハンバーグメニュー（英語）
+$(function(){
+	$(".en_nav_toggle").on("click", function () {
+		$(".en_nav_toggle, .en_nav").toggleClass("show")
+		$(".select").toggleClass("top_fil")
+	})
+});
+//ハンバーグメニュー（中国語）
+$(function(){
+	$(".zh_nav_toggle").on("click", function () {
+		$(".zh_nav_toggle, .zh_nav").toggleClass("show")
+		$(".select").toggleClass("top_fil")
+	})
+});
+
